6fecefc2271c3be2af56cbd3eac8872e036537b9
Suman Mandal <38155383+gitsumanmandal@users.noreply.github.com>
2018-04-29 01:41:32 +0530

    Master

--- app.js
+++ app.js
@@ -16,6 +16,6 @@ app.use(express.static(path.join(__dirname, 'frontend')));
 
 routes(app);
 
-var server = app.listen(app.get(port), function () {
-    console.log("app running on port.", server.address().port);
-});
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'))
+})
